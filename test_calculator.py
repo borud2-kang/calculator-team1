626a6580673270030d97ef374dbde560c207d157
Yongjin Lee <yong7.lee@samsung.com>
2026-06-30 16:16:56 +0900

    add getZegop

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,13 @@
 import pytest
-
+from calculator import Calc
 
 def test_sample():
     assert 1 == 1
     pytest.fail()
+
+def test_getZegop():
+    cal = Calc()
+
+    result = cal.getZegop(2)
+
+    assert result == 4
